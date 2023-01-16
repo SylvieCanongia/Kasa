a51377ece32073b75e851a2da70b1025c3caac80
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2023-01-16 10:00:16 +0100

    :sparkles: feat: dynamic @copyright in the footer component

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Kasa is a training project around the rental of apartments for individuals
 
-Kasa est un projet de formation autour de la location d'appartements pour les particuliers
+Kasa est un projet de formation autour de la location d'appartements pour les particuliers.
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,13 +2,14 @@ import './footer.scss';
 import LogoFooter from './logo-footer.svg'
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <div className="footer">
       <div className="footerLogo">
         <img src={LogoFooter} alt="Kasa logo" />
       </div>
       <p className='footerCopyright'>
-      © 2020 Kasa. All rights reserved
+      © { currentYear } Kasa. All rights reserved
       </p>
     </div>
   );
